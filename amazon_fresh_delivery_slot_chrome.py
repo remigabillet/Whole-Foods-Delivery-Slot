7af47100aa6db3ead78478875610506da5a0344a
Remi Gabillet <remi@envoy.com>
2020-04-14 22:01:11 -0700

    Improving whole_foods_delivery_slot_chrome to be more readable, keep repeating when a slot is available, and notify when the page has changed

--- amazon_fresh_delivery_slot_chrome.py
+++ amazon_fresh_delivery_slot_chrome.py
@@ -1,7 +1,6 @@
 import bs4
 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 
 import sys
 import time
@@ -9,55 +8,84 @@ import os
 
 
 def getWFSlot(productUrl):
-   headers = {
-       'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
-   }
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+    }
 
-   driver = webdriver.Chrome()
-   driver.get(productUrl)           
-   html = driver.page_source
-   soup = bs4.BeautifulSoup(html)
-   time.sleep(60)
-   no_open_slots = True
+    driver = webdriver.Chrome()
+    driver.get(productUrl)           
+    html = driver.page_source
+    soup = bs4.BeautifulSoup(html)
+    time.sleep(60)
+    duration = 5
+    no_open_slots = True
 
-   while no_open_slots:
-      driver.refresh()
-      print("refreshed")
-      html = driver.page_source
-      soup = bs4.BeautifulSoup(html)
-      time.sleep(2)
+#    while no_open_slots:
+#       driver.refresh()
+#       print("refreshed")
+#       html = driver.page_source
+#       soup = bs4.BeautifulSoup(html)
+#       time.sleep(2)
 
-      no_open_slots = "No doorstep delivery windows are available for"
-      try:
-         no_slots_from_web = driver.find_element_by_xpath('/html/body/div[5]/div/div/div[2]/div/div/form/div[3]/div[4]/div/div[2]/div[2]/div[6]/div/div[2]/div/div[2]/div/div[20]/div[1]/div[1]/div/div/div/span').text
-         if no_open_slots in no_slots_from_web:
-            pass
-         else:
-            print('SLOTS OPEN!')
-            os.system('say "Slots for delivery opened!"')
-            no_open_slots = False
-            time.sleep(1400)
-      except NoSuchElementException:
-         print('SLOTS OPEN!')
-         os.system('say "Slots for delivery opened!"')
-         no_open_slots = False
-         time.sleep(1400)
+#       no_open_slots = "No doorstep delivery windows are available for"
+#       try:
+#          no_slots_from_web = driver.find_element_by_xpath('/html/body/div[5]/div/div/div[2]/div/div/form/div[3]/div[4]/div/div[2]/div[2]/div[6]/div/div[2]/div/div[2]/div/div[20]/div[1]/div[1]/div/div/div/span').text
+#          if no_open_slots in no_slots_from_web:
+#             continue
+#          else:
+#             print('SLOTS OPEN!')
+#             os.system('say "Slots for delivery opened!"')
+#             no_open_slots = False
+#             time.sleep(1400)
+#       except AttributeError:
+#          print('SLOTS OPEN!')
+#          os.system('say "Slots for delivery opened!"')
+#          no_open_slots = False
+#          time.sleep(1400)
 
 
-      try:
-         open_slots = soup.find('div', class_ ='orderSlotExists').text()
-         if open_slots != "false":
-            print('SLOTS OPEN!')
-            os.system('say "Slots for delivery opened!"')
-            no_open_slots = False
-            time.sleep(1400)
-      except AttributeError:
-         pass
+#       try:
+#          open_slots = soup.find('div', class_ ='orderSlotExists').text()
+#          if open_slots != "false":
+#             print('SLOTS OPEN!')
+#             os.system('say "Slots for delivery opened!"')
+#             no_open_slots = False
+#             time.sleep(1400)
+#       except AttributeError:
+#          continue
 
+    while True:
+        if no_open_slots:
+            print("Refreshing")
+            driver.refresh()
+            html = driver.page_source
+            soup = bs4.BeautifulSoup(html, 'html.parser')
+
+            # soup.find_all('div', class_='ServiceType-slot-container')[0].find('span', class_="a-size-base-plus").string.strip()
+            try:
+                # soup.find_all('button', id=re.compile('date-button-'))
+                # for day_button in soup.find_all('div', class_='ufss-date-select-toggle-text-container'):
+                for slot in soup.find_all('div', class_='ServiceType-slot-container'):
+                    # print(f"day button: {day_button}")
+                    for availability in slot.find('span', class_="a-size-base-plus"):
+                        availability = availability.string.strip()
+                        print(f"availability: {availability}")
+                        # for status in availability.stripped_strings:
+                        #     print(f"status: {status}")
+                        if status != 'Not available':
+                            print('SLOTS OPEN!')
+                            os.system('say "Slots for delivery opened!"')
+                            no_open_slots = False
+                if no_open_slots:
+                    sys.stdout.write('\rRefreshed at {}.'.format(time.strftime('%H:%M:%S', time.localtime())))
+                    sys.stdout.flush()
+                    time.sleep(duration)
+            except AttributeError as error:
+                print('error: {0}'.format(error))
+                continue
+        else:
+            time.sleep(duration)
       
-
-      
-
 
 getWFSlot('https://www.amazon.com/gp/buy/shipoptionselect/handlers/display.html?hasWorkingJavascript=1')
 
--- whole_foods_delivery_slot_chrome.py
+++ whole_foods_delivery_slot_chrome.py
@@ -5,6 +5,27 @@ from selenium import webdriver
 import sys
 import time
 import os
+import random
+
+
+from datetime import datetime
+
+
+def log(msg):
+   now = datetime.now().strftime("%H:%M:%S")
+   print(f"{now} | {msg}")
+
+
+def sleep_countdown(total_seconds):
+   seconds_left = total_seconds
+   log(f"Sleep for {seconds_left} seconds ...")
+   while seconds_left > 0:
+      time.sleep(1)
+      seconds_left -= 1
+
+def slots_are_open():
+   log(' ************* SLOTS OPEN **************')
+   os.system('say "Slots for delivery opened!"')
 
 
 def getWFSlot(productUrl):
@@ -15,51 +36,46 @@ def getWFSlot(productUrl):
    driver = webdriver.Chrome()
    driver.get(productUrl)           
    html = driver.page_source
-   soup = bs4.BeautifulSoup(html, "html.parser")
-   time.sleep(60)
+   soup = bs4.BeautifulSoup(html)
+
+   log("Script started.")
+   sleep_countdown(45)
    no_open_slots = True
 
+
    while no_open_slots:
+      # log("Refreshing..")
       driver.refresh()
-      print("refreshed")
+
+      page = driver.current_url
+
+      if 'buy/shipoptionselect' in page:
+         page = 'shipoptionselect'
+      else:
+         os.system('say "on the wrong page"')
+
+      log('')
+      log(f'Refreshed {page}')
+
       html = driver.page_source
-      soup = bs4.BeautifulSoup(html, "html.parser")
-      time.sleep(4)
+      soup = bs4.BeautifulSoup(html)
+      
+      # METHOD 1
+      element = soup.find('h4', class_ ='ufss-slotgroup-heading-text a-text-normal')
+      if element and 'Next available' in element.text:
+         slots_are_open()
+         continue
 
-      try:
-         slot_opened_text = "Not available"
-         all_dates = soup.findAll("div", {"class": "ufss-date-select-toggle-text-availability"})
-         for each_date in all_dates:
-            if slot_opened_text not in each_date.text:
-               print('SLOTS OPEN 2!')
-               os.system('say "Slots for delivery opened!"')
-               no_open_slots = False
-               time.sleep(1400)
-      except AttributeError:
-         pass
+      # METHOD 2
+      all_dates = soup.findAll("div", {"class": "ufss-date-select-toggle-text-availability"})
+      for each_date in all_dates:
+         if "Not available" not in each_date.text:
+            slots_are_open()
+            continue
 
-      try:
-         no_slot_pattern = 'No delivery windows available. New windows are released throughout the day.'
-         if no_slot_pattern == soup.find('h4', class_ ='a-alert-heading').text:
-            print("NO SLOTS!")
-      except AttributeError: 
-            print('SLOTS OPEN 3!')
-            os.system('say "Slots for delivery opened!"')
-            no_open_slots = False
-
-
-      slot_patterns = ['Next available', '1-hour delivery windows', '2-hour delivery windows']
-      try:
-         next_slot_text = str([x.text for x in soup.findAll('h4', class_ ='ufss-slotgroup-heading-text a-text-normal')])
-         if any(next_slot_text in slot_pattern for slot_pattern in slot_patterns):
-            print('SLOTS OPEN!')
-            winsound.Beep(freq, duration)
-            no_open_slots = False
-
-            autoCheckout(driver)
-            
-      except AttributeError:
-         pass
+      log("NO SLOTS :(")
+      
+      sleep_countdown(random.randrange(2)+7)
 
 
 getWFSlot('https://www.amazon.com/gp/buy/shipoptionselect/handlers/display.html?hasWorkingJavascript=1')
